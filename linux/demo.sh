bd6c18d8fbcd9c6743eefe86d31ca6bdc8fe29d3
Yinlin Hu <huyinlin@gmail.com>
2018-06-15 14:33:52 +0200

    Update demo.sh

--- linux/demo.sh
+++ linux/demo.sh
@@ -1,2 +1,2 @@
-./RIC "../data/KITTI/000008_10.png"  "../data/KITTI/000008_11.png" "../data/KITTI/000008_10_match.txt"
-./RIC "../data/MPI-Sintel/frame_0001.png"  "../data/MPI-Sintel/frame_0002.png" "../data/MPI-Sintel/frame_0001_match.txt"
+RIC "../data/KITTI/000008_10.png"  "../data/KITTI/000008_11.png" "../data/KITTI/000008_10_match.txt"
+RIC "../data/MPI-Sintel/frame_0001.png"  "../data/MPI-Sintel/frame_0002.png" "../data/MPI-Sintel/frame_0001_match.txt"
